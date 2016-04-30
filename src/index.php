1303b369045864a4a17e91ee875aa23fe85529bb
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2016-04-30 17:30:56 +0100

    Updated to work with new files
    Added support for new files, also added search and new meta tags

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
+// UPUO 1.0B https://github.com/jake-cryptic/ultrapowa-user-overviews
 require_once("config.inc.php");
+require_once("database.class.php");
 
 if ($isOnline = @fsockopen($conf->meta->server_ip,$conf->meta->server_port,$errno,$errstr,$conf->meta->check_timeout)) {
 	fclose($isOnline);
@@ -8,8 +10,9 @@ if ($isOnline = @fsockopen($conf->meta->server_ip,$conf->meta->server_port,$errn
 	$serverStatus = '<span style="color:red; font-weight: bold;">Offline</span>';
 }
 
-$dbc = new databaseConnection();
-$dbc->connect();
+$Database = new Database();
+$Database->config = $conf;
+$Database->Connect();
 
 if (empty($_GET["page"])) {
 	$currentPage = 0;
@@ -24,7 +27,7 @@ if (empty($_GET["page"])) {
 	}
 }
 
-if (empty($_GET["sortBy"])) {
+if (!isset($_GET["sortBy"])) {
 	$sql = "SELECT * FROM player LIMIT $currentPageLimit,".$conf->meta->content_limit;
 } else {
 	$sortBy = htmlentities($_GET["sortBy"]);
@@ -47,6 +50,14 @@ if (empty($_GET["sortBy"])) {
 		case "ut_desc":
 			$sql = "SELECT * FROM player ORDER BY `LastUpdateTime` DESC LIMIT $currentPageLimit,".$conf->meta->content_limit;
 			break;
+		case "search":
+			if (isset($_GET["search"])) {
+				$search = $Database->Esc($_GET["search"]);
+				$sql = "SELECT * FROM player WHERE `PlayerId` LIKE '%{$search}%' LIMIT $currentPageLimit,".$conf->meta->content_limit;
+			} else {
+				die("<h1>Search Query Not Set</h1>");
+			}
+			break;
 		default:
 			$sql = "SELECT * FROM player LIMIT $currentPageLimit,".$conf->meta->content_limit;
 			break;
@@ -60,7 +71,9 @@ if (empty($_GET["sortBy"])) {
 	
 		<title><?php echo $conf->meta->site_name; ?></title>
 		<meta charset="<?php echo $conf->meta->charset; ?>">
-		<meta name="viewport" content="width=device-width, maximum-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="content-language" content="<?php echo $conf->meta->default_lang; ?>" />
 		
 		<style>
 		body { width:98%; margin: 0% .5% 0% 1.5%; }
@@ -77,14 +90,22 @@ if (empty($_GET["sortBy"])) {
 		<div id="container">
 			<div class="row">
 				<div class="col-xs-8 col-sm-6 col-md-10 col-lg-10">
-					<h1><?php echo $conf->meta->site_name . " " . $serverStatus; ?></h1>
+					<h1><?php echo $conf->meta->site_name . " - " . $serverStatus; ?></h1>
 				</div>
 			</div>
-		
+			
+			<?php
+				$response = $Database->Run($sql);
+				if (!$response) {
+					die($Database->Error());
+				}
+			?>
+			
 			<table class="table table-striped table-bordered">
 				<thead>
 					<tr>
-						<th><h3>Player ID <a href="?sortBy=id_asc" title="ID ascending">▲</a><a href="?sortBy=id_desc" title="ID descending">▼</a></h3></th>
+						<th><h3>Player ID <a href="?sortBy=id_asc" title="ID ascending">▲</a><a href="?sortBy=id_desc" title="ID descending">▼</a>
+						<form action="index.php" method="GET"><input type="hidden" value="search" name="sortBy" /><input type="text" name="search" placeholder="Search PlayerID" /><button>Go</button></form></h3></th>
 						<th><h3>Privilege Level <a href="?sortBy=pl_asc" title="Privilege Level ascending">▲</a><a href="?sortBy=pl_desc" title="Privilege Level descending">▼</a></h3></th>
 						<th><h3>Last Update Time <a href="?sortBy=ut_asc" title="LUT ascending">▲</a><a href="?sortBy=ut_desc" title="LUT descending">▼</a></h3></th>
 						<th><h3>Edit User</h3></th>
@@ -92,21 +113,24 @@ if (empty($_GET["sortBy"])) {
 				</thead>
 				<tbody>
 				<?php
-				if (!$dbc->queryDB($sql)) {
-					echo $dbc->errors()."<br />";
-				} else {
-					$databaseResponse = $dbc->queryDB($sql);
-					while($row = mysqli_fetch_array($databaseResponse)){ //Construct bootstrap table with data
-						$avatarDecoded = json_decode($row["Avatar"]);
-						$playerName = $avatarDecoded->{"avatar_name"};
-						
-						echo "<tr>
-						<td>" . $row["PlayerId"] . "</td>
-						<td>" . $row["AccountPrivileges"] . "</td>
-						<td>" . $row["LastUpdateTime"] . "</td>
-						<td><a title=\"Edit User\" href=\"user.php?id=" . $row["PlayerId"] . "\">" . $playerName . " ►</a></td></tr></a>\n";
+					if ($response->num_rows == 0) {
+						if (isset($search)) {
+							echo "<tr><td colspan=\"4\" style=\"text-align:center;font-size:2em;\">No results for search '{$search}'</td></tr>";
+						} else {
+							echo "<tr><td colspan=\"4\" style=\"text-align:center;font-size:2em;\">No results</td></tr>";
+						}
+					} else {
+						while($row = mysqli_fetch_array($response)){ //Construct bootstrap table with data
+							$avatarDecoded = json_decode($row["Avatar"]);
+							$playerName = $avatarDecoded->{"avatar_name"};
+							
+							echo "<tr>
+							<td>" . $row["PlayerId"] . "</td>
+							<td>" . $row["AccountPrivileges"] . "</td>
+							<td>" . $row["LastUpdateTime"] . "</td>
+							<td><a title=\"Edit User\" href=\"user.php?id=" . $row["PlayerId"] . "\">" . $playerName . " ►</a></td></tr></a>\n";
+						}
 					}
-				}
 				?>
 				</tbody>
 			</table>
@@ -124,4 +148,4 @@ if (empty($_GET["sortBy"])) {
 		</div>
 	</body>
 </html>
-<?php $dbc->disconnect(); ?>
+<?php $Database->Disconnect(); ?>
