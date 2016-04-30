6bc734931057796b11caf0cb92593ac23c5218c2
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2016-04-30 17:31:36 +0100

    New settings

--- src/config.inc.php
+++ src/config.inc.php
@@ -1,21 +1,19 @@
 <?php
-require("database.class.php");
-
+// UPUO 1.0B https://github.com/jake-cryptic/ultrapowa-user-overviews
 $conf = (object) array(
 	"meta" => (object) array(
-		"site_name" => "Your server name here ", // Website/server name
-		"server_ip" => "127.0.0.1", // Game Servers IP
-		"server_port" => "9339", // Port to check for the server on
-		"check_timeout" => "(float)1.5", // Timeout when checking the server status(bigger the float, longer the load time)
-		"default_lang" => "en", // The default language
-		"charset" => "UTF-16", // The site's charset for each page
-		"content_limit" => "20"), // This limits the amount of content to be shown from the database per page
-	"files" => (object) array (
-		"libs" => (object) array( // Links to Libraries used in the website
-			"bootstrap-css" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css", // Bootstrap CSS
-			"bootstrap-js" => "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js", // Bootstrap JS
-			"jquery" => "https://code.jquery.com/jquery-2.1.4.min.js") // JQuery
-		)
-	);
-	
+		"DEBUG" => false,				// Show SQL errors or not
+		"allowEdits" => false,				// Allow editing users from the site
+		
+		"site_name" => "Server Name", 			// Website/server name
+		"server_ip" => "127.0.0.1", 			// Game Servers IP
+		"server_port" => "9339", 			// Port to check for the server on
+		"check_timeout" => (float)1.5, 			// Timeout when checking the server status(bigger the float, longer the load time)
+		"default_lang" => "en", 			// The default language
+		"charset" => "UTF-8", 				// The site's charset for each page
+		"content_limit" => "20"				// This limits the amount of content to be shown from the database per page
+	)
+);
+
+global $conf;
 ?>
